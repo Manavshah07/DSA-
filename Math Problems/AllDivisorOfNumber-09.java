790ca198f02af50c2c10413567a77e314f24f71b
Manavshah07 <shahmanav197@gmail.com>
2025-12-29 20:56:11 +0530

    initial commit

--- Math Problems/AllDivisorOfNumber-09.java
+++ Math Problems/AllDivisorOfNumber-09.java
@@ -3,6 +3,18 @@
 
 import java.util.*;
 
+// TLE
+// class Solution {
+//     public static void print_divisors(int n) {
+//         // code here
+
+//         for(int i=1; i<=n; i++) {
+//             if(n % i == 0)
+//                 System.out.print(i + " ");
+//         }
+//     }
+// }
+
 class Solution {
     public static void print_divisors(int n) {
         // code here
---
+++ Sliding window and 2 pointers/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+// BRUTE FORCE : TIME COMPLEXITY:-O(N^2) AND SPACE COMPLEXITY:- O(N) BECAUSE USING HASHSET
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int n = s.length();
+        int maxLen = 0;
+
+        // starting index
+        for (int i = 0; i < n; i++) {
+            HashSet<Character> set = new HashSet<>();
+
+            // ending index
+            for (int j = i; j < n; j++) {
+
+                // if character already exists, stop
+                if (set.contains(s.charAt(j))) {
+                    break;
+                }
+
+                // add character to set
+                set.add(s.charAt(j));
+
+                // update maximum length
+                maxLen = Math.max(maxLen, j - i + 1);
+            }
+        }
+        return maxLen;
+    }
+}
+
+// OPTIMAL APPROACH : TIME COMPLEXITY:-O(N) AND SPACE COMPLEXITY:- O(N) BECAUSE
+// USING HASHMAP
+
+// class Solution {
+// public int lengthOfLongestSubstring(String s) {
+// int n = s.length();
+// int ans = 0;
+// int i=0;
+// int j=0;
+// HashMap<Character, Integer> hmap = new HashMap<>();
+
+// while(j < n) {
+// char ch = s.charAt(j);
+// hmap.put(ch, hmap.getOrDefault(ch, 0) + 1);
+
+// if(hmap.size() == j-i+1) {
+// // HERE WE GOT THE UNIQUE CHARACTERS
+// ans = Math.max(ans, j-i+1);
+// j++;
+// } else if(hmap.size() < j-i+1) {
+// // HERE DUPLICATE CHARACTER OCCURS IN MY MAP
+// while(hmap.size() < j-i+1) {
+// char ch1 = s.charAt(i);
+// hmap.put(ch1, hmap.get(ch1) - 1);
+
+// if(hmap.get(ch1) == 0)
+// hmap.remove(ch1);
+// i++;
+// }
+// j++;
+// }
+// }
+// return ans;
+// }
+// }
